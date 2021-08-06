ae7cd8e162a717e826b10887dc8c4f3c8d032445
Hanns Holger Rutz <contact@sciss.de>
2021-08-07 01:33:39 +0200

    ... <>

--- src/main/scala/de/sciss/nvim/CaseClassExample.scala
+++ src/main/scala/de/sciss/nvim/CaseClassExample.scala
@@ -6,30 +6,30 @@ import scodec.msgpack._
 import scodec.{Attempt, Codec, DecodeResult}
 
 sealed abstract class Setting extends Product with Serializable
-final case class IsCompact(v: Boolean) extends Setting
-final case class SchemaSize(v: Int) extends Setting
+final case class IsCompact  (v: Boolean ) extends Setting
+final case class SchemaSize (v: Int     ) extends Setting
 final case class MsgPackExample(compact: IsCompact, schema: SchemaSize)
 
 object CaseClassExample {
   def main(args: Array[String]): Unit = run()
 
   def run(): Unit = {
-    val input = MsgPackExample(IsCompact(true), SchemaSize(123))
-    roundTrip(input)
+    val in = MsgPackExample(IsCompact(true), SchemaSize(123))
+    roundTrip(in)
     println("Ok")
   }
 
-  def roundTrip[A](a: A)(implicit C: Codec[A]): Unit = {
-    C.encode(a) match {
-      case Attempt.Failure(error) =>
-        sys.error(error.toString())
-      case Attempt.Successful(encoded) =>
-        C.decode(encoded) match {
-          case Attempt.Failure(error) =>
-            sys.error(error.toString())
-          case Attempt.Successful(DecodeResult(decoded, remainder)) =>
-            assert(remainder === BitVector.empty)
-            assert(decoded == a)
+  def roundTrip[A](a: A)(implicit cdc: Codec[A]): Unit = {
+    cdc.encode(a) match {
+      case Attempt.Failure(e) =>
+        sys.error(e.toString)
+      case Attempt.Successful(enc) =>
+        cdc.decode(enc) match {
+          case Attempt.Failure(e) =>
+            sys.error(e.toString)
+          case Attempt.Successful(DecodeResult(dec, rem)) =>
+            assert(rem === BitVector.empty)
+            assert(dec == a)
         }
     }
   }
